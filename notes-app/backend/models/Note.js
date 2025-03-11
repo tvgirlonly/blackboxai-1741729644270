d35916482ace0d958a46eb385a2058bd8f41afd1
jessica joseph <102388289+tvgirlonly@users.noreply.github.com>
2025-03-12 03:18:21 +0530

    Add notes-app/backend/models/Note.js 1741729701300

---
+++ notes-app/backend/models/Note.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const noteSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User'
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  }
+}, {
+  timestamps: true
+});
+
+const Note = mongoose.model('Note', noteSchema);
+module.exports = Note;
